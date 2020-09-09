242cf3c0fae93de9a16f935028ed0a7d6c8d7fcb
ahsanzakaullah <52410753+ahsanzakaullah@users.noreply.github.com>
2020-09-09 22:23:51 +0500

    Add project files.

---
+++ Programming.Tasks/MindStromTasks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Programming.Tasks
+{
+    public static class MindStromTasks
+    {
+        public static void PrintFactorial(int number)
+        {
+            for (int i = number - 1; i > 0; i--)
+            {
+                number *= i;
+            }
+            Console.WriteLine(number);
+        }
+
+        public static void PrintTriangle(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        public static void PrintStar(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = i; j < 2*number-i; j++)
+                {
+                    for (int k = 0; k <j; k++)
+                    {
+                        Console.Write("* ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Programming.Tasks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Programming.Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide the number for factorial");
+            int val= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Factorial of {0} is .. ",val);
+            MindStromTasks.PrintFactorial(val);
+            MindStromTasks.PrintTriangle(val);
+        }
+    }
+}
